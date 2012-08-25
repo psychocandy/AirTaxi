4e1712a16d949b2331c4b64c793d6e4a5a17de82
Yosy Attias <yosy101@gmail.com>
2012-08-25 20:20:09 +0300

    Moved to backbone js, initial get ride screen

---
+++ client/src/views.js
@@ -0,0 +1,16 @@
+$(function(){
+	window.AirTaxi = window.AirTaxi || {};
+	window.AirTaxi.Views = window.AirTaxi.Views || {};
+
+	window.AirTaxi.Views.GetRideView = Backbone.View.extend({
+		el : $("#get_ride"),
+		initialize: function(){
+			alert(345354);
+		}
+	});
+
+
+});
+
+
+
