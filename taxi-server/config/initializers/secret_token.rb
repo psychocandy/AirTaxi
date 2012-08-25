2359e8935be5353c6d576f542c100acb31e85842
Amir Friedman <github@null.co.il>
2012-08-25 16:04:58 +0300

    initial server commit

---
+++ taxi-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaxiServer::Application.config.secret_token = 'ed0b72fb9a2ee05b3ff8cb6dadc7639dd13bede88d21dc5cdd5e450d3b63c91282b384e3a8c8707b709ab174fc9d1d422ee6c1228def467b67ca251cd6d84aa6'
